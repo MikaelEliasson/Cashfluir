abb8023a305697fbcc324526ba857cbc7504a512
Ola Martner <omartner@hotmail.com>
2011-08-13 22:46:45 +0200

    Created controller and views for creating a user

---
+++ Cashfluir.Web/Controllers/UserController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cashfluir.Web.Models;
+
+namespace Cashfluir.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ICommandInvoker invoker;
+
+        public UserController(ICommandInvoker invoker)
+        {
+            this.invoker = invoker;
+        }
+
+        public UserController()
+        {
+
+        }
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /User/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /User/Create
+
+        public ActionResult Create()
+        {
+            return View(new CreateUserViewModel());
+        } 
+
+        //
+        // POST: /User/Create
+
+        [HttpPost]
+        public ActionResult Create(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dummy = model.Name;
+
+                this.invoker.Execute(dummy);
+                return RedirectToAction("Index");
+
+            }
+
+            return View(model);
+        }
+        
+        //
+        // GET: /User/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /User/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Cashfluir.Web/Models/CreateUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cashfluir.Web.Models
+{
+    public class CreateUserViewModel
+    {
+        [Required]
+        [StringLength(3)]
+        public string Name { get; set; }
+    }
+}
